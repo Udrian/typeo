13537a3d041649d0248073077d401ae930a5ebf5
Simon 'Udrian' Haggren <simon@typedeaf.com>
2019-09-09 17:50:38 +0200

    Added the TypeOCommon source

---
+++ TypeOCommon/Common/Plane.cs
@@ -0,0 +1,14 @@
+﻿namespace TypeO.Common
+{
+    public class Plane
+    {
+        public Vec3 Normal;
+        public float D;
+
+        public Plane(Vec3 normal, float d) {
+            Normal = normal;
+            Normal.Normalize();
+            D = d;
+        }
+    }
+}
--- TypeOCommon/Main.cs
+++
@@ -1,9 +0,0 @@
-namespace TestProject
-{
-    public static class Test
-    {
-        public static void Init()
-        {
-        }
-    }
-}
