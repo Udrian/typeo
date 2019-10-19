571ee24f3dc80a36bc5cec1d41f9223ce23b36be
Simon 'Udrian' Haggren <simon@typedeaf.com>
2019-10-19 22:28:03 +0200

    Split out TypeORunner from TypeO

--- TypeOCore/Typedeaf/TypeOCore/TypeO.cs
+++ TypeOCore/Typedeaf/TypeOCore/TypeO.cs
@@ -7,48 +7,10 @@ namespace Typedeaf.TypeOCore
 {
     public partial class TypeO
     {
-        public partial class Runner<T> where T : Game
-        {
-            protected TypeO TypeO { get; private set; }
-
-            public Runner()
-            {
-                TypeO = new TypeO();
-            }
-
-            public TypeO.Runner<T> SetKeyAlias(object input, object key)
-            {
-                TypeO.KeyConverter.SetKeyAlias(input, key);
-                return this;
-            }
-
-            public void Start()
-            {
-                //Initialize the game
-                var game = (T)Activator.CreateInstance(typeof(T), TypeO);
-                TypeO.Game = game;
-                game.Initialize();
-
-                TypeO.Start();
-
-                //Cleanup
-                foreach(var module in TypeO.Modules)
-                {
-                    module.Cleanup();
-                }
-            }
-        }
-
         private TypeO() {
             Modules = new List<Module>();
             KeyConverter = new KeyConverter(this);
             LastTick = DateTime.UtcNow;
-        }
-
-        public static TypeO.Runner<T> Create<T>() where T : Game
-        {
-            var factory = new TypeO.Runner<T>();
-            return factory;
         }
 
         public bool Exit { get; set; } = false;
---
+++ TypeOCore/Typedeaf/TypeOCore/TypeORunner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Typedeaf.TypeOCore.Input;
+
+namespace Typedeaf.TypeOCore
+{
+    public partial class TypeO
+    {
+        public partial class Runner<T> where T : Game
+        {
+            protected TypeO TypeO { get; private set; }
+
+            public Runner()
+            {
+                TypeO = new TypeO();
+            }
+
+            public TypeO.Runner<T> SetKeyAlias(object input, object key)
+            {
+                TypeO.KeyConverter.SetKeyAlias(input, key);
+                return this;
+            }
+
+            public void Start()
+            {
+                //Initialize the game
+                var game = (T)Activator.CreateInstance(typeof(T), TypeO);
+                TypeO.Game = game;
+                game.Initialize();
+
+                TypeO.Start();
+
+                //Cleanup
+                foreach(var module in TypeO.Modules)
+                {
+                    module.Cleanup();
+                }
+            }
+        }
+
+        public static TypeO.Runner<T> Create<T>() where T : Game
+        {
+            var factory = new TypeO.Runner<T>();
+            return factory;
+        }
+    }
+}
