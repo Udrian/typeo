5968105123edf7bc2b16eaba050eed0b0c937cec
Simon 'Udrian' Haggren <simon@typedeaf.com>
2019-11-04 20:51:06 +0100

    removed SDLDrawableTexture and moved IIsSDLDrawable from IIsSDLDrawable.cs to SDLDrawable.cs

---
+++ TypeOSDL/Typedeaf/TypeOSDL/Entities/Drawables/SDLDrawable.cs
@@ -0,0 +1,10 @@
+using Typedeaf.TypeOCore.Entities.Drawables;
+using Typedeaf.TypeOSDL.Graphics;
+
+namespace Typedeaf.TypeOSDL
+{
+    namespace Entities
+    {
+        public interface IIsSDLDrawable : IIsDrawable<SDLCanvas> { }
+    }
+}
