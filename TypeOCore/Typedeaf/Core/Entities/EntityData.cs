3455a00218adaa57005827e9d87e42d665772deb
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-01-29 23:32:34 +0100

    Added EntityData

---
+++ TypeOCore/Typedeaf/Core/Entities/EntityData.cs
@@ -0,0 +1,12 @@
+﻿namespace TypeOEngine.Typedeaf.Core
+{
+    namespace Entities
+    {
+        public abstract class EntityData
+        {
+            public EntityData() { }
+
+            public abstract void Initialize();
+        }
+    }
+}
---
+++ TypeOCore/Typedeaf/Core/Entities/Interfaces/IHasData.cs
@@ -0,0 +1,16 @@
+﻿namespace TypeOEngine.Typedeaf.Core
+{
+    namespace Entities.Interfaces
+    {
+        public interface IHasData
+        {
+            public EntityData Data { get; set; }
+        }
+
+        public interface IHasData<D> : IHasData where D : EntityData
+        {
+            EntityData IHasData.Data { get => Data; set => Data = (D)value; }
+            public new D Data { get; set; }
+        }
+    }
+}
