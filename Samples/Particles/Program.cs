5b8a4dea2be3db9442e15c28d96b09071bac3dff
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-03-02 22:59:49 +0100

    Added Particles sample game

---
+++ Samples/Particles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TypeOEngine.Typedeaf.Core.Engine;
+using TypeOEngine.Typedeaf.Desktop;
+using TypeOEngine.Typedeaf.SDL;
+
+namespace Particles
+{
+    class Program
+    {
+        static void Main()
+        {
+            TypeO.Create<ParticlesGame>("Space invader")
+                 .LoadModule<DesktopModule>(new DesktopModuleOption() { SaveLogsToDisk = false })
+                 .LoadModule<SDLModule>()
+                 .SetLogger(LogLevel.Info)
+                 .Start();
+        }
+    }
+}
