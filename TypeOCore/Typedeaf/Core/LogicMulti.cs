66c35b81dfec827820fb9d740ed7fc24261dac36
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-03-06 21:40:05 +0100

    Fixed the remove logic function in LogicMulti

--- TypeOCore/Typedeaf/Core/LogicMulti.cs
+++ TypeOCore/Typedeaf/Core/LogicMulti.cs
@@ -14,16 +14,18 @@ namespace TypeOEngine.Typedeaf.Core
         private Context Context { get => (this as IHasContext).Context; set => (this as IHasContext).Context = value; }
 
         protected List<Logic> Logics { get; set; }
+        private List<Logic> RemoveLogics { get; set; }
         public Scene Scene { get; set; }
         public Entity Entity { get; set; }
         public EntityData EntityData { get; set; }
 
-        public LogicMulti() { }
-
-        public override void Initialize()
+        public LogicMulti()
         {
             Logics = new List<Logic>();
+            RemoveLogics = new List<Logic>();
         }
+
+        public override void Initialize() { }
 
         public override void Update(double dt)
         {
@@ -31,6 +33,9 @@ namespace TypeOEngine.Typedeaf.Core
             {
                 logic.Update(dt);
             }
+
+            RemoveLogics.ForEach(l => Logics.Remove(l));
+            RemoveLogics.Clear();
         }
 
         public L CreateLogic<L>() where L : Logic, new()
@@ -47,7 +52,10 @@ namespace TypeOEngine.Typedeaf.Core
 
         public bool RemoveLogic<L>() where L : Logic
         {
-            return Logics.Remove(Logics.FirstOrDefault(l => l.GetType() == typeof(L)));
+            var l = Logics.FirstOrDefault(l => l.GetType() == typeof(L));
+            if (l == null) return false;
+            RemoveLogics.Add(l);
+            return true;
         }
 
         public L GetLogic<L>() where L : Logic
