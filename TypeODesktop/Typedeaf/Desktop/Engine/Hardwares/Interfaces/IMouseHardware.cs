34b43fd739273d1d39022e904eb7c67b5d52210b
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-04-05 09:37:37 +0200

    Added mouse interface in IMouseHardware

--- TypeODesktop/Typedeaf/Desktop/Engine/Hardwares/Interfaces/IMouseHardware.cs
+++ TypeODesktop/Typedeaf/Desktop/Engine/Hardwares/Interfaces/IMouseHardware.cs
@@ -1,4 +1,5 @@
-﻿using TypeOEngine.Typedeaf.Core.Engine.Hardwares.Interfaces;
+﻿using TypeOEngine.Typedeaf.Core.Common;
+using TypeOEngine.Typedeaf.Core.Engine.Hardwares.Interfaces;
 
 namespace TypeOEngine.Typedeaf.Desktop
 {
@@ -6,6 +7,15 @@ namespace TypeOEngine.Typedeaf.Desktop
     {
         public interface IMouseHardware : IHardware
         {
+            public Vec2 CurrentMousePosition { get; set; }
+            public Vec2 OldMousePosition { get; set; }
+            public Vec2 CurrentWheelPosition { get; set; }
+            public Vec2 OldWheelPosition { get; set; }
+
+            public bool CurrentButtonDownEvent(object key);
+            public bool CurrentButtonUpEvent(object key);
+            public bool OldButtonDownEvent(object key);
+            public bool OldButtonUpEvent(object key);
         }
     }
 }
--- TypeOSDL/Typedeaf/SDL/Engine/Hardwares/SDLMouseHardware.cs
+++ TypeOSDL/Typedeaf/SDL/Engine/Hardwares/SDLMouseHardware.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using TypeOEngine.Typedeaf.Core.Common;
 using TypeOEngine.Typedeaf.Core.Engine.Hardwares;
 using TypeOEngine.Typedeaf.Desktop.Engine.Hardwares.Interfaces;
 using TypeOEngine.Typedeaf.SDL.Engine.Hardwares.Interfaces;
@@ -9,8 +10,17 @@ namespace TypeOEngine.Typedeaf.SDL
     {
         public class SDLMouseHardware : Hardware, IMouseHardware, ISDLEvents
         {
+            public Vec2 CurrentMousePosition { get; set; }
+            public Vec2 OldMousePosition { get; set; }
+            public Vec2 CurrentWheelPosition { get; set; }
+            public Vec2 OldWheelPosition { get; set; }
+
             public override void Initialize()
             {
+                CurrentMousePosition = new Vec2();
+                OldMousePosition = new Vec2();
+                CurrentWheelPosition = new Vec2();
+                OldWheelPosition = new Vec2();
             }
 
             public override void Cleanup()
@@ -18,6 +28,41 @@ namespace TypeOEngine.Typedeaf.SDL
             }
 
             public void UpdateEvents(List<SDL2.SDL.SDL_Event> events)
+            {
+                OldMousePosition = new Vec2(CurrentMousePosition);
+                OldWheelPosition = new Vec2(CurrentWheelPosition);
+                foreach (var e in events)
+                {
+                    if(e.type == SDL2.SDL.SDL_EventType.SDL_MOUSEWHEEL)
+                    {
+                        CurrentWheelPosition += new Vec2(e.wheel.x, e.wheel.y);
+                    }
+                    else if(e.type == SDL2.SDL.SDL_EventType.SDL_MOUSEMOTION)
+                    {
+                        CurrentMousePosition = new Vec2(e.motion.x, e.motion.y);
+                    }
+                    else if(e.type == SDL2.SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN)
+                    {
+                    }
+                }
+            }
+
+            public bool CurrentButtonDownEvent(object key)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public bool CurrentButtonUpEvent(object key)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public bool OldButtonDownEvent(object key)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public bool OldButtonUpEvent(object key)
             {
                 throw new System.NotImplementedException();
             }
