69867c53cf66eac60495db0214d12fad0f85ec61
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-04-29 21:19:29 +0200

    Logic now has the IsUpdatable interface

--- TypeOCore/Typedeaf/Core/Logic.cs
+++ TypeOCore/Typedeaf/Core/Logic.cs
@@ -1,7 +1,11 @@
-﻿namespace TypeOEngine.Typedeaf.Core
+﻿using TypeOEngine.Typedeaf.Core.Interfaces;
+
+namespace TypeOEngine.Typedeaf.Core
 {
-    public abstract class Logic
+    public abstract class Logic : IIsUpdatable
     {
+        public bool Pause { get; set; }
+
         public abstract void Initialize();
         public abstract void Update(double dt);
     }
