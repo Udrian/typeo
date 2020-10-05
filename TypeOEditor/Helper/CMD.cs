5f513846796acbbe728b0a7fc64f78ef77244bc1
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-10-05 20:30:29 +0200

    Added the process to a task

--- TypeOEditor/Helper/CMD.cs
+++ TypeOEditor/Helper/CMD.cs
@@ -1,10 +1,16 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace TypeOEditor.Helper
 {
     public static class CMD
     {
+        private static Process Process { get; set; }
+        private static Task ProcessTask { get; set; }
+        private static Queue<string> Cmds { get; set; } = new Queue<string>();
+
         public static void Run(string cmd)
         {
             Run(new string[] { cmd });
@@ -12,26 +18,39 @@ namespace TypeOEditor.Helper
 
         public static void Run(string[] cmds)
         {
-            using (var p = new Process())
+            foreach(var cmd in cmds)
             {
+                Cmds.Enqueue(cmd);
+            }
+
+            if(Process == null || Process.HasExited)
+            {
+                Process = new Process();
                 var info = new ProcessStartInfo();
                 info.FileName = "cmd.exe";
                 info.RedirectStandardInput = true;
                 info.UseShellExecute = false;
 
-                p.StartInfo = info;
-                p.Start();
-
-                using (StreamWriter sw = p.StandardInput)
+                Process.StartInfo = info;
+                Process.Start();
+            }
+            if(ProcessTask == null || ProcessTask.IsCompleted)
+            {
+                ProcessTask = new Task(() =>
                 {
-                    foreach (var cmd in cmds)
+                    using (StreamWriter sw = Process.StandardInput)
                     {
-                        if (sw.BaseStream.CanWrite)
+                        while (Cmds.Count > 0)
                         {
-                            sw.WriteLine(cmd);
+                            var cmd = Cmds.Dequeue();
+                            if (sw.BaseStream.CanWrite)
+                            {
+                                sw.WriteLine(cmd);
+                            }
                         }
                     }
-                }
+                });
+                ProcessTask.Start();
             }
         }
     }
