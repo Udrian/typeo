fb5de8257934b742a036a5a1fd8fbcc8d86a695d
Simon 'Udrian' Haggren <simon@typedeaf.com>
2020-12-20 10:01:18 +0100

    Added version paramaters to build

--- Scripts/build.py
+++ Scripts/build.py
@@ -3,22 +3,31 @@ import argparse, sys, os
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--project', type=str, required=True, help="Project name to build")
+    parser.add_argument('-b', '--build', type=int, required=True, help="Build number to append to versioning")
     parser.add_argument('-c', '--config', type=str, default="Debug", help="Debug|Release defaults to Debug")
     parser.add_argument('-o', '--output', type=str, help="Output folder")
     args = parser.parse_args()
 
-    build(args.project, args.config, args.output)
+    build(args.project, args.build, args.config, args.output)
 
-def build(project, config="Debug", output=None):
-    print("Building '{}' with config '{}' to output '{}'".format(project, config, output))
-
+def build(project, build_number, config="Debug", output=None):
     path = "../{0}/{0}.csproj".format(project)
 
     if(output == None or output == ""):
         output = "../bin/builds/{}/{}".format(project, config)
 
+    version = "{}.{}".format(getVersion(project), build_number)
+
+    print("Building '{}' '{}' '{}' to output '{}'".format(project, config, version, output))
+
     os.system("dotnet clean {} --configuration {} --output {} --verbosity n".format(path, config, output))
-    os.system("dotnet publish {} --configuration {} --output {} --verbosity n".format(path, config, output))
+    os.system("dotnet publish {} --configuration {} --output {} --verbosity n /property:Version={}".format(path, config, output, version))
+
+def getVersion(project):
+    path = "../{0}/version".format(project)
+
+    with open(path) as f:
+        return f.readline().replace("\n", "").replace("\r", "").replace("v", "")
 
 if __name__ == "__main__":
     main()
--- Scripts/build_all.py
+++ Scripts/build_all.py
@@ -1,15 +1,21 @@
 import build
+import argparse, sys, os
 
 configs = ["Debug", "Release"]
 modules = ["TypeOCore", "TypeODesktop", "TypeOSDL"]
 
 def main():
-    build_modules()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--build', type=int, required=True, help="Build number to append to versioning")
+    parser.add_argument('-o', '--output', type=str, help="Output folder")
+    args = parser.parse_args()
 
-def build_modules():
+    build_modules(args.build, args.output)
+
+def build_modules(build_number, output=None):
     for module in modules:
         for config in configs:
-            build.build(module, config)
+            build.build(module, build_number, config, output)
 
 if __name__ == "__main__":
     main()
