fc9f8891b467fc9fa71b853a566c5fb0cf7b0bc8
Simon 'Udrian' Haggren <simon@typedeaf.com>
2021-01-24 18:01:25 +0100

    Added TextAdventure project

---
+++ Samples/TextAdventure/TextAdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using TypeOEngine.Typedeaf.Core;
+
+namespace TextAdventure
+{
+    class TextAdventureGame : Game
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override void Draw()
+        {
+        }
+
+        public override void Update(double dt)
+        {
+        }
+        public override void Cleanup()
+        {
+        }
+    }
+}
