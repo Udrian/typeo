385225869688278e3b554122af57bdb96542060f
Simon 'Udrian' Haggren <simon@typedeaf.com>
2021-09-03 13:13:11 +0200

    Added Splash screen

---
+++ Editor/TypeDitor/Splash.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TypeDitor
+{
+    /// <summary>
+    /// Interaction logic for Splash.xaml
+    /// </summary>
+    public partial class Splash : Window
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Create new Project
+            OpenMainWindow();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Open old Project
+            OpenMainWindow();
+        }
+
+        private void OpenMainWindow()
+        {
+            var mainWindow = new MainWindow();
+            Application.Current.MainWindow = mainWindow;
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
