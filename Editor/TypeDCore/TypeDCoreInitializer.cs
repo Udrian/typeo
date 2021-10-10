dce6c5968da45796fb46ed4472efedd47540ed2a
Simon 'Udrian' Haggren <simon@typedeaf.com>
2021-10-10 22:37:32 +0200

    More init cleanup

--- Editor/TypeDCore/TypeDCoreInitializer.cs
+++ Editor/TypeDCore/TypeDCoreInitializer.cs
@@ -1,7 +1,11 @@
 ﻿using TypeD;
 using TypeD.Models.Data.Hooks;
 using TypeD.Models.Interfaces;
+using TypeD.Types;
+using TypeDCore.Code.Drawable2d;
+using TypeDCore.Code.Entity;
 using TypeDCore.Code.Game;
+using TypeDCore.Code.Scene;
 
 namespace TypeDCore
 {
@@ -15,6 +19,14 @@ namespace TypeDCore
             ProjectModel = Resources.Get<IProjectModel>("ProjectModel");
 
             Hooks.AddHook("ProjectCreate", ProjectCreate);
+
+            TypeOType.AddTypeOType(typeof(TypeOEngine.Typedeaf.Core.Game), typeof(GameTypeOType));
+            TypeOType.AddTypeOType(typeof(TypeOEngine.Typedeaf.Core.Scene), typeof(SceneTypeOType));
+            TypeOType.AddTypeOType(typeof(TypeOEngine.Typedeaf.Core.Entities.Entity), typeof(EntityTypeOType));
+            TypeOType.AddTypeOType(typeof(TypeOEngine.Typedeaf.Core.Entities.Drawables.Drawable), typeof(Drawable2dTypeOType));
+            //TypeOType.AddTypeOType(typeof(Stub), typeof());
+            //TypeOType.AddTypeOType(typeof(Logic), typeof());
+            //TypeOType.AddTypeOType(typeof(EntityData), typeof());
         }
 
         void ProjectCreate(object param)
