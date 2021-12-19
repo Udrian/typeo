52d719fcc2e6605e5d15bc226165ed0accfdfe6a
Simon 'Udrian' Haggren <simon@typedeaf.com>
2021-12-19 11:34:51 +0100

    continued

--- Editor/TypeDSDL/View/Documents/SDLViewerDocument.xaml.cs
+++ Editor/TypeDSDL/View/Documents/SDLViewerDocument.xaml.cs
@@ -2,13 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms.Integration;
 using System.Windows.Interop;
 using TypeD.Models.Data;
-using TypeD.Viewer;
 using TypeDSDL.Viewer;
 using TypeOEngine.Typedeaf.Desktop;
 using TypeOEngine.Typedeaf.SDL;
@@ -21,92 +19,72 @@ namespace TypeDSDL.View.Documents
     public partial class SDLViewerDocument : UserControl
     {
 		SDLFakeGameViewer Viewer { get; set; }
+		System.Windows.Forms.Panel WinFormsGamePanel { get; set; }
+		WindowsFormsHost WindowsFormsHostGamePanel { get; set; }
 
-		// These are the variables you care about.
-		private System.Windows.Forms.Panel WinFormsGamePanel { get; set; }
-		private WindowsFormsHost WindowsFormsHostGamePanel { get; set; }
+		Project Project { get; set; }
+		TypeOType TypeOType { get; set; }
 
-		// IGNORE MEEEEE
-		private IntPtr glContext;
-		private delegate void Viewport(int x, int y, int width, int height);
-		private Viewport glViewport;
-
-		[DllImport("user32.dll")]
-		private static extern IntPtr SetWindowPos(
-			IntPtr handle,
-			IntPtr handleAfter,
-			int x,
-			int y,
-			int cx,
-			int cy,
-			uint flags
-		);
 		[DllImport("user32.dll")]
 		private static extern IntPtr SetParent(IntPtr child, IntPtr newParent);
-		[DllImport("user32.dll")]
-		private static extern IntPtr ShowWindow(IntPtr handle, int command);
 
 		public SDLViewerDocument(Project project, TypeOType typeOType)
         {
             InitializeComponent();
 
-            if (typeOType.TypeOBaseType == "Drawable2d")
+			Project = project;
+			TypeOType = typeOType;
+			Viewer = new SDLFakeGameViewer(Project, new List<Tuple<TypeOEngine.Typedeaf.Core.Engine.Module, TypeOEngine.Typedeaf.Core.Engine.ModuleOption>>()
             {
-				Viewer = new SDLFakeGameViewer(project, typeOType, new List<TypeOEngine.Typedeaf.Core.Engine.Module>()
-                {
-                    new DesktopModule(),
-                    new SDLModule()
-                });
-				this.Loaded += SDLViewerDocument_Loaded;
-			}
+				new Tuple<TypeOEngine.Typedeaf.Core.Engine.Module, TypeOEngine.Typedeaf.Core.Engine.ModuleOption>(
+					new DesktopModule(), null),
+				new Tuple<TypeOEngine.Typedeaf.Core.Engine.Module, TypeOEngine.Typedeaf.Core.Engine.ModuleOption>(
+					new SDLModule(), new SDLModuleOption() { WindowFlags =  SDL2.SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL2.SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE})
+            });
+			this.Loaded += SDLViewerDocument_Loaded;
+            this.SizeChanged += SDLViewerDocument_SizeChanged;
+		}
+
+        private void SDLViewerDocument_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+			if (WinFormsGamePanel == null || WindowsFormsHostGamePanel == null) return;
+			SetWindowSize(e.NewSize);
+		}
+
+		private void SetWindowSize(Size size)
+		{
+			WinFormsGamePanel.Size = new System.Drawing.Size((int)size.Width, (int)size.Height);
+			WindowsFormsHostGamePanel.Width = size.Width;
+			WindowsFormsHostGamePanel.Height = size.Height;
+
+			var source = PresentationSource.FromVisual(this);
+			var transformToDevice = source.CompositionTarget.TransformToDevice;
+			var pixelSize = (Size)transformToDevice.Transform((Vector)size);
+
+			Viewer.SetWindowSize(new TypeOEngine.Typedeaf.Core.Common.Vec2((int)pixelSize.Width, (int)pixelSize.Height));
 		}
 
         private void SDLViewerDocument_Loaded(object sender, RoutedEventArgs e)
         {
-			// This is what we're going to attach the SDL2 window to
+			if (ActualWidth == 0 || ActualHeight == 0) return;
 			WinFormsGamePanel = new System.Windows.Forms.Panel();
-			var ah = this.ActualHeight;
-			WinFormsGamePanel.Size = new System.Drawing.Size((int)ActualWidth, (int)ActualHeight);
-
-			// Make the WinForms window
-			WindowsFormsHostGamePanel = new WindowsFormsHost() { Child = WinFormsGamePanel, Width = WinFormsGamePanel.Size.Width, Height = WinFormsGamePanel.Size.Height };
+			WindowsFormsHostGamePanel = new WindowsFormsHost() { Child = WinFormsGamePanel };
 			Canvas.Children.Add(WindowsFormsHostGamePanel);
 
-			// IGNORE MEEEEE
-			var windowPtr = Viewer.WindowHandler;
-			/*glContext = SDL.SDL_GL_CreateContext(windowPtr);
-			glViewport = (Viewport)Marshal.GetDelegateForFunctionPointer(
-				SDL.SDL_GL_GetProcAddress("glViewport"),
-				typeof(Viewport)
-			);
-			glViewport(0, 0, WinFormsGamePanel.Size.Width, WinFormsGamePanel.Size.Height);*/
+			Viewer.Start();
+			Viewer.AddTypeOType(Project, TypeOType);
+			SetWindowSize(new Size(ActualWidth, ActualHeight));
 
-			// Get the Win32 HWND from the SDL2 window
+			var windowPtr = Viewer.WindowHandler;
 			SDL.SDL_SysWMinfo info = new SDL.SDL_SysWMinfo();
 			SDL.SDL_GetWindowWMInfo(windowPtr, ref info);
 			IntPtr winHandle = info.info.win.window;
 
-			// Move the SDL2 window to 0, 0
-			SetWindowPos(
-				winHandle,
-				new WindowInteropHelper(Application.Current.MainWindow).Handle,
-				0,
-				0,
-				0,
-				0,
-				0x0401 // NOSIZE | SHOWWINDOW
-			);
-
-			// Attach the SDL2 window to the panel
 			SetParent(winHandle, WinFormsGamePanel.Handle);
-			ShowWindow(winHandle, 1); // SHOWNORMAL
 		}
 
 		private void UserControl_Unloaded(object sender, RoutedEventArgs e)
         {
-			glViewport = null;
-			SDL.SDL_GL_DeleteContext(glContext);
-			glContext = IntPtr.Zero;
 			Viewer.Close();
 		}
     }
--- Editor/TypeDSDL/Viewer/SDLFakeGameViewer.cs
+++ Editor/TypeDSDL/Viewer/SDLFakeGameViewer.cs
@@ -25,7 +25,11 @@ namespace TypeDSDL.Viewer
 
             public override void Initialize()
             {
-                Window = WindowService.CreateWindow("Test", new Vec2(0, 0), new Vec2(640, 480), false, true);
+            }
+
+            public void CreateWindowAndCanvas(Vec2 size)
+            {
+                Window = WindowService.CreateWindow("Test", new Vec2(0, 0), size, false, true);
                 Canvas = WindowService.CreateCanvas(Window);
             }
 
@@ -35,6 +39,7 @@ namespace TypeDSDL.Viewer
 
             public override void Draw()
             {
+                if (Canvas == null) return;
                 Canvas.Clear(Color.Black);
                 foreach (var drawable in Drawables.Get<Drawable>())
                 {
@@ -53,32 +58,53 @@ namespace TypeDSDL.Viewer
         public IntPtr WindowHandler { get { return ((SDLWindow)Game.Window).SDL_Window; } }
         public IntPtr CanvasHandler { get { return ((SDLCanvas)Game.Canvas).SDLRenderer; } }
 
-        public SDLFakeGameViewer(Project project, TypeOType drawable, List<TypeOEngine.Typedeaf.Core.Engine.Module> modules = null)
+        public SDLFakeGameViewer(Project project, List<Tuple<TypeOEngine.Typedeaf.Core.Engine.Module, TypeOEngine.Typedeaf.Core.Engine.ModuleOption>> modules = null)
         {
-            if (drawable.TypeOBaseType != "Drawable2d") return;
-            var typeInfo = project.Assembly.GetType(drawable.FullName);
-            if (typeInfo == null) return;
-
             FakeTypeO = TypeO.Create<FakeGame>("Drawable Viewer") as TypeO;
             if (modules != null)
             {
                 foreach (var module in modules)
                 {
-                    FakeTypeO.LoadModule(module);
+                    FakeTypeO.LoadModule(module.Item1, module.Item2);
                 }
             }
-            var task = new Task(() =>
+        }
+
+        public void Start()
+        {
+            Task.Run(() =>
             {
                 FakeTypeO.Start();
             });
-            task.Start();
 
             while (!(FakeTypeO?.Context?.Game?.Initialized == true))
             {
                 Task.Delay(100);
             }
             Game = FakeTypeO.Context.Game as FakeGame;
-            Game.Drawables.Create(typeInfo);
+        }
+
+        public void AddTypeOType(Project project, TypeOType type)
+        {
+            var typeInfo = project.Assembly.GetType(type.FullName);
+            if (typeInfo == null) return;
+            if (type.TypeOBaseType == "Drawable2d")
+            {
+                Game.Drawables.Create(typeInfo);
+            }
+        }
+
+        public void SetWindowSize(Vec2 size)
+        {
+            if(Game.Window == null)
+            {
+                Game.CreateWindowAndCanvas(size);
+            }
+            else
+            {
+                Game.Window.Size = size;
+                Game.Canvas.Viewport = new Rectangle(Game.Canvas.Viewport.Pos, size);
+            }
         }
 
         public void Close()
